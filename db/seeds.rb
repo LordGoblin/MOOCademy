b3b85cd713c16cacfb0000bc797d50216f2d918e
LordGoblin <maxime78600@gmail.com>
2019-01-30 23:26:42 +0100

    ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,3 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-require 'faker'
-i = 0
-5.times do
-	i = i + 1
-	3.times do
-  		lecon = Lecon.create!(cour_id: i.to_i, title: Faker::Cannabis.type, body: Faker::Cannabis.health_benefit)
-	end
-end
